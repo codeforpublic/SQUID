3a1d76203f34d4e6f893f356aef064c9a1cc1ac6
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2020-03-26 05:51:22 +0700

    fix metro bug

--- metro.config.js
+++ metro.config.js
@@ -7,7 +7,7 @@
 
 const path = require('path');
 module.exports = {
-  projectRoot: path.resolve(__dirname, '../../'),
+  projectRoot: path.resolve(__dirname),
   transformer: {
     getTransformOptions: async () => ({
       transform: {
