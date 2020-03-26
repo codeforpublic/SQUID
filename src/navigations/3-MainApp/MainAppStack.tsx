fcf62950d04455312d52d9613c068af43061dfb2
Witchayut <skpepe134@gmail.com>
2020-03-26 21:42:44 +0700

    fix tab bar text

--- src/navigations/3-MainApp/MainAppStack.tsx
+++ src/navigations/3-MainApp/MainAppStack.tsx
@@ -5,13 +5,26 @@ import { QRCodeResult } from './QRCodeResult'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import AntIcon from 'react-native-vector-icons/AntDesign'
 import { createBottomTabNavigator } from 'react-navigation'
-import { COLORS } from '../../styles'
+import { COLORS, FONT_FAMILY } from '../../styles'
+import { Text } from 'react-native'
 
 export const MainAppTab = createBottomTabNavigator({
   MainApp: {
     screen: MainApp,
     navigationOptions: {
-      label: 'ข้อมูล',
+      tabBarLabel: ({ focused }) => (
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 12,
+            fontFamily: FONT_FAMILY,
+            marginBottom: 2,
+            color: focused ? COLORS.PRIMARY_DARK : COLORS.GRAY_2,
+          }}
+        >
+          ข้อมูล
+        </Text>
+      ),
       tabBarIcon: ({ focused }) => (
         <Icon
           name="user"
@@ -24,7 +37,19 @@ export const MainAppTab = createBottomTabNavigator({
   QRCodeScan: {
     screen: QRCodeScan,
     navigationOptions: {
-      label: 'สแกน QR',
+      tabBarLabel: ({ focused }) => (
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 12,
+            fontFamily: FONT_FAMILY,
+            marginBottom: 2,
+            color: focused ? COLORS.PRIMARY_DARK : COLORS.GRAY_2,
+          }}
+        >
+          สแกน QR
+        </Text>
+      ),
       tabBarIcon: ({ focused }) => (
         <AntIcon
           name="scan1"
