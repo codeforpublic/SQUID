aec77fd584d64d51522d38ee50155ed4c58cb439
Witchayut <skpepe134@gmail.com>
2020-03-26 21:37:51 +0700

    add tab

---
+++ src/components/WhiteBackground.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View } from 'react-native'
+import { COLORS } from '../styles'
+
+export const WhiteBackground = ({ children }) => {
+  return (
+    <View style={{ backgroundColor: COLORS.WHITE, flex: 1 }}>{children}</View>
+  )
+}
