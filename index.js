6314a70daa0916a872515e5d5a10bce80ecaa828
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2020-03-27 11:52:00 +0700

    ios icon & lgo

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-import "react-native-gesture-handler"
+import 'react-native-gesture-handler'
 import 'react-native-get-random-values'
-import * as Sentry from "@sentry/react-native";
-import { AppRegistry } from 'react-native';
-import App from './src/App';
+import * as Sentry from '@sentry/react-native'
+import { AppRegistry, Text } from 'react-native'
+import App from './src/App'
 
-import BackgroundGeolocation from "./src/react-native-background-geolocation";
+import BackgroundGeolocation from './src/react-native-background-geolocation'
+
+Text.defaultProps = Text.defaultProps || {}
+Text.defaultProps.allowFontScaling = false
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
-    dsn: "https://9ba1ba3df3174145b8d276c9d84b6b52@sentry.io/5175128"
+    dsn: 'https://9ba1ba3df3174145b8d276c9d84b6b52@sentry.io/5175128',
   })
 }
 
-
 // Make BackgroundGeolocation API global for handy access in Javascript Debugger console
-global.BackgroundGeolocation = BackgroundGeolocation;
+global.BackgroundGeolocation = BackgroundGeolocation
 
-import BackgroundFetch from "react-native-background-fetch";
+import BackgroundFetch from 'react-native-background-fetch'
 
-AppRegistry.registerComponent('FightCovid19', () => App);
+AppRegistry.registerComponent('FightCovid19', () => App)
 
 /**
-* BackgroundGeolocation Headless JS task.
-* For more information, see:  https://github.com/transistorsoft/react-native-background-geolocation/wiki/Android-Headless-Mode
-*/
-let BackgroundGeolocationHeadlessTask = async (event) => {
-  let params = event.params;
-  console.log('[BackgroundGeolocation HeadlessTask] -', event.name, params);
+ * BackgroundGeolocation Headless JS task.
+ * For more information, see:  https://github.com/transistorsoft/react-native-background-geolocation/wiki/Android-Headless-Mode
+ */
+let BackgroundGeolocationHeadlessTask = async event => {
+  let params = event.params
+  console.log('[BackgroundGeolocation HeadlessTask] -', event.name, params)
 
   switch (event.name) {
     case 'heartbeat':
@@ -38,19 +40,18 @@ let BackgroundGeolocationHeadlessTask = async (event) => {
       });
       console.log('[BackgroundGeolocation HeadlessTask] - getCurrentPosition:', location);
       */
-      break;
+      break
   }
 }
 
-
-BackgroundGeolocation.registerHeadlessTask(BackgroundGeolocationHeadlessTask);
+BackgroundGeolocation.registerHeadlessTask(BackgroundGeolocationHeadlessTask)
 
 /**
-* BackgroundFetch Headless JS Task.
-* For more information, see:  https://github.com/transistorsoft/react-native-background-fetch#config-boolean-enableheadless-false
-*/
-let BackgroundFetchHeadlessTask = async (event) => {
-  console.log('[BackgroundFetch HeadlessTask] start');
+ * BackgroundFetch Headless JS Task.
+ * For more information, see:  https://github.com/transistorsoft/react-native-background-fetch#config-boolean-enableheadless-false
+ */
+let BackgroundFetchHeadlessTask = async event => {
+  console.log('[BackgroundFetch HeadlessTask] start')
   // Important:  await asychronous tasks when using HeadlessJS.
   /* DISABLED
   let location = await BackgroundGeolocation.getCurrentPosition({persist: false, samples: 1});
@@ -59,11 +60,10 @@ let BackgroundFetchHeadlessTask = async (event) => {
   // If you don't do this, your app could be terminated and/or assigned
   // battery-blame for consuming too much time in background.
   */
-  console.log('[BackgroundFetch HeadlessTask] finished');
+  console.log('[BackgroundFetch HeadlessTask] finished')
 
-  BackgroundFetch.finish();
+  BackgroundFetch.finish()
 }
 
-
 // Register your BackgroundFetch HeadlessTask
-BackgroundFetch.registerHeadlessTask(BackgroundFetchHeadlessTask);
+BackgroundFetch.registerHeadlessTask(BackgroundFetchHeadlessTask)
