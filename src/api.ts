bc69573643f3ee496178ae939b7d72dff2223b77
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2020-03-31 03:32:47 +0700

    production

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import { userPrivateData } from './state/userPrivateData'
 import nanoid from 'nanoid'
 
-export const API_URL = 'https://api.staging.thaialert.com'
+export const API_URL = 'https://api.thaialert.com'
+// export const API_URL = 'https://api.staging.thaialert.com'
 // export const API_URL = 'http://192.168.1.102:4210'
 const API_KEY = 'd6857fca1cfbeb600b399ac29f2dabf9'
 
