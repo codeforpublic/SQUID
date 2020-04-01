678ad49473c3cd31ef96d46479cab23e42ca30fe
Sittiphol Phanvilai <sittiphol@gmail.com>
2020-03-31 18:00:02 -0700

    [React Bridge] Setup structure

---
+++ ContactTracer/ContactTracerReact/src/User.js
@@ -0,0 +1,29 @@
+import {AsyncStorage} from '@react-native-community/async-storage';
+import 'react-native-get-random-values';
+import {nanoid} from './nanoid';
+
+var _userId = '';
+
+export async function getUserId() {
+  if (_userId != '') return _userId;
+  try {
+    const value = await AsyncStorage.getItem('userId');
+    if (value !== null) {
+      _userId = value;
+      return _userId;
+    }
+  } catch (error) {
+    // Error retrieving data
+  }
+  _userId = nanoid().substring(0, 20);
+  await saveUserId(_userId);
+  return _userId;
+}
+
+async function saveUserId(userId) {
+  try {
+    await AsyncStorage.setItem('userId', userId);
+  } catch (error) {
+    // Error saving data
+  }
+}
