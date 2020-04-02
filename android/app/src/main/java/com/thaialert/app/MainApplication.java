6d6487f7519cc8a9e9e3ec9c55b9b254e2dcf319
Witchayut <skpepe134@gmail.com>
2020-04-02 14:05:54 +0700

    use .env with react-native-config

--- android/app/src/main/java/com/thaialert/app/MainApplication.java
+++ android/app/src/main/java/com/thaialert/app/MainApplication.java
@@ -6,6 +6,7 @@ import android.os.StrictMode;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
