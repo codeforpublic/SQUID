bf8072c867dd6399fa39ae180c0d9cf07a2d3d1c
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2020-04-02 21:29:28 +0700

    react-native-config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,10 @@
 import Config from 'react-native-config'
+/*
+  define config in .env and .env.production
+*/
 
-export const API_URL = Config.API_URL
+export const API_URL = Config.API_URL || 'https://api.staging.thaialert.com'
+
+setTimeout(() => {
+  console.log('Config', Config)
+}, 1000)
