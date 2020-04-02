64d4baa6add808ad58a01aa4e65013c5dedfbd94
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2020-04-02 22:30:45 +0700

    unlink push-noti (use autolink)

--- android/app/src/main/java/com/thaialert/app/MainApplication.java
+++ android/app/src/main/java/com/thaialert/app/MainApplication.java
@@ -7,7 +7,6 @@ import android.os.StrictMode;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.imageeditor.ImageEditorPackage;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
