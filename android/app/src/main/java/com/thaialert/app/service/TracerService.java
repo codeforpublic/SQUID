e91dfda4687e3457e48f31824934d55c336772d3
Sittiphol Phanvilai <sittiphol@gmail.com>
2020-04-03 20:47:13 -0700

    [Android] Do healthcheck when task is removed

--- android/app/src/main/java/com/thaialert/app/service/TracerService.java
+++ android/app/src/main/java/com/thaialert/app/service/TracerService.java
@@ -141,12 +141,14 @@ public class TracerService extends Service {
     public void onTaskRemoved(Intent rootIntent) {
         Intent restartServiceTask = new Intent(getApplicationContext(), this.getClass());
         restartServiceTask.setPackage(getPackageName());
-        PendingIntent restartPendingIntent =PendingIntent.getService(getApplicationContext(), 1, restartServiceTask, PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent restartPendingIntent = PendingIntent.getService(getApplicationContext(), 1, restartServiceTask, PendingIntent.FLAG_ONE_SHOT);
         AlarmManager myAlarmService = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
         myAlarmService.set(
                 AlarmManager.ELAPSED_REALTIME,
                 SystemClock.elapsedRealtime() + 1000,
                 restartPendingIntent);
+
+        broadcastHealthCheck();
 
         super.onTaskRemoved(rootIntent);
     }
