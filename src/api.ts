725ef09bd1c01adbf09bccad10081cf8e65fb1a1
Sittiphol Phanvilai <sittiphol@gmail.com>
2020-04-05 18:16:17 -0700

    Fix app unable to run

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,8 @@
 import DeviceInfo from 'react-native-device-info'
 import { userPrivateData } from './state/userPrivateData'
 import nanoid from 'nanoid'
-// import { API_URL } from './config'
-const API_URL = 'http://192.168.1.102:4210'
+import { API_URL } from './config'
+// const API_URL = 'http://192.168.1.102:4210'
 
 const API_KEY = 'd6857fca1cfbeb600b399ac29f2dabf9'
 
@@ -39,7 +39,7 @@ export const registerDevice = async (): Promise<{
   if (!result.anonymousId || !result.userId) {
     throw new Error('RegisterDevice failed')
   }
-  
+
   return { userId: result.userId, anonymousId: result.anonymousId }
 }
 
