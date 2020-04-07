a3702a16a0d654493b13a6e01940547446d44afb
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2020-04-07 16:54:31 +0700

    contact scanner

--- src/services/contact-scanner.ts
+++ src/services/contact-scanner.ts
@@ -10,9 +10,11 @@ class ScanManager {
   locationAccuracy: number
   latestUploadTS?: number
   oldestItemTS?: number
-  constructor({ ttl, locationAccuracy }) {
+  type: string
+  constructor({ ttl, locationAccuracy, type }) {
     this.locationAccuracy = locationAccuracy
     this.ttl = ttl
+    this.type = type
     let prevState
     AppState.addEventListener('change', state => {
       if (prevState !== state) {
@@ -71,9 +73,11 @@ class ScanManager {
 
 export const scanManager = new ScanManager({
   ttl: 30 * 1000,
+  type: 'qrscan',
   locationAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
 })
-export const slowPaceScanner = new ScanManager({
+export const bluetoothScanner = new ScanManager({
   ttl: 10 * 60 * 1000,
+  type: 'bluetooth',
   locationAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_LOW,
 })
