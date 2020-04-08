3ccd388ba40f59ec15a2b2b5ca5ea6390d9015ec
nuttapol.k <nuttapol_k@cleverse.com>
2020-04-08 22:16:17 +0700

    add padding top in tab menu bar

--- src/navigations/3-MainApp/MainAppStack.tsx
+++ src/navigations/3-MainApp/MainAppStack.tsx
@@ -13,108 +13,117 @@ import { COLORS, FONT_FAMILY } from '../../styles'
 import { Text } from 'react-native'
 import { MainAppFaceCamera } from './MainAppFaceCamera'
 
-export const MainAppTab = createBottomTabNavigator({
-  MainApp: {
-    screen: MainApp,
-    navigationOptions: {
-      tabBarLabel: ({ focused }) => (
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 12,
-            fontFamily: FONT_FAMILY,
-            marginBottom: 2,
-            color: focused ? '#303342' : COLORS.GRAY_2,
-          }}
-        >
-          ข้อมูล
-        </Text>
-      ),
-      tabBarIcon: ({ focused }) => (
-        <Icon
-          name="user"
-          color={focused ? '#303342' : COLORS.GRAY_2}
-          size={20}
-        />
-      ),
+export const MainAppTab = createBottomTabNavigator(
+  {
+    MainApp: {
+      screen: MainApp,
+      navigationOptions: {
+        tabBarLabel: ({ focused }) => (
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 12,
+              fontFamily: FONT_FAMILY,
+              marginBottom: 2,
+              color: focused ? '#303342' : COLORS.GRAY_2,
+            }}
+          >
+            ข้อมูล
+          </Text>
+        ),
+        tabBarIcon: ({ focused }) => (
+          <Icon
+            name="user"
+            color={focused ? '#303342' : COLORS.GRAY_2}
+            size={20}
+          />
+        ),
+      },
+    },
+    QRCodeScan: {
+      screen: QRCodeScan,
+      navigationOptions: {
+        tabBarLabel: ({ focused }) => (
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 12,
+              fontFamily: FONT_FAMILY,
+              marginBottom: 2,
+              color: focused ? '#303342' : COLORS.GRAY_2,
+            }}
+          >
+            สแกน QR
+          </Text>
+        ),
+        tabBarIcon: ({ focused }) => (
+          <AntIcon
+            name="scan1"
+            color={focused ? '#303342' : COLORS.GRAY_2}
+            size={20}
+          />
+        ),
+      },
+    },
+    Settings: {
+      screen: Settings,
+      navigationOptions: {
+        tabBarLabel: ({ focused }) => (
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 12,
+              fontFamily: FONT_FAMILY,
+              marginBottom: 2,
+              color: focused ? '#303342' : COLORS.GRAY_2,
+            }}
+          >
+            ตั้งค่า
+          </Text>
+        ),
+        tabBarIcon: ({ focused }) => (
+          <AntIcon
+            name="profile"
+            color={focused ? '#303342' : COLORS.GRAY_2}
+            size={20}
+          />
+        ),
+      },
+    },
+    // Debug: {
+    //   screen: Debug,
+    //   navigationOptions: {
+    //     tabBarLabel: ({ focused }) => (
+    //       <Text
+    //         style={{
+    //           textAlign: 'center',
+    //           fontSize: 12,
+    //           fontFamily: FONT_FAMILY,
+    //           marginBottom: 2,
+    //           color: focused ? '#303342' : COLORS.GRAY_2,
+    //         }}
+    //       >
+    //         Debug
+    //       </Text>
+    //     ),
+    //     tabBarIcon: ({ focused }) => (
+    //       <AntIcon
+    //         name="BugOutlined"
+    //         color={focused ? '#303342' : COLORS.GRAY_2}
+    //         size={20}
+    //       />
+    //     ),
+    //   },
+    // },
+  },
+  {
+    tabBarOptions: {
+      style: {
+        paddingTop: 3,
+      },
     },
   },
-  QRCodeScan: {
-    screen: QRCodeScan,
-    navigationOptions: {
-      tabBarLabel: ({ focused }) => (
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 12,
-            fontFamily: FONT_FAMILY,
-            marginBottom: 2,
-            color: focused ? '#303342' : COLORS.GRAY_2,
-          }}
-        >
-          สแกน QR
-        </Text>
-      ),
-      tabBarIcon: ({ focused }) => (
-        <AntIcon
-          name="scan1"
-          color={focused ? '#303342' : COLORS.GRAY_2}
-          size={20}
-        />
-      ),
-    },
-  },
-  Settings: {
-    screen: Settings,
-    navigationOptions: {
-      tabBarLabel: ({ focused }) => (
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 12,
-            fontFamily: FONT_FAMILY,
-            marginBottom: 2,
-            color: focused ? '#303342' : COLORS.GRAY_2,
-          }}
-        >
-          ตั้งค่า
-        </Text>
-      ),
-      tabBarIcon: ({ focused }) => (
-        <AntIcon
-          name="profile"
-          color={focused ? '#303342' : COLORS.GRAY_2}
-          size={20}
-        />
-      ),
-    },
-  },
-  // Debug: {
-  //   screen: Debug,
-  //   navigationOptions: {
-  //     tabBarLabel: ({ focused }) => (
-  //       <Text
-  //         style={{
-  //           textAlign: 'center',
-  //           fontSize: 12,
-  //           fontFamily: FONT_FAMILY,
-  //           marginBottom: 2,
-  //           color: focused ? '#303342' : COLORS.GRAY_2,
-  //         }}
-  //       >
-  //         Debug
-  //       </Text>
-  //     ),
-  //     tabBarIcon: ({ focused }) => (
-  //       <AntIcon
-  //         name="BugOutlined"
-  //         color={focused ? '#303342' : COLORS.GRAY_2}
-  //         size={20}
-  //       />
-  //     ),
-  //   },
-  // },
-})
+)
 
 export const MainAppStack = createStackNavigator(
   {
