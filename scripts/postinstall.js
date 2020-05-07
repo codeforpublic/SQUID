15145d440f55bba61d1f477747aed64817a3f5bd
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2020-05-08 06:09:53 +0700

    add automate script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,16 +1,19 @@
 const fs = require('fs')
 
-const acExists = fs.existsSync(
-  __dirname + '/../ios/ThaiAlert/AppCenter-Config.plist',
-)
-if (!acExists) {
+if (!fs.existsSync(__dirname + '/../ios/ThaiAlert/AppCenter-Config.plist')) {
   fs.copyFileSync(
     __dirname + '/../ios/ThaiAlert/AppCenter-Config.plist.example',
     __dirname + '/../ios/ThaiAlert/AppCenter-Config.plist',
   )
 }
 
-const envExists = fs.existsSync(__dirname + '/../.env')
-if (!envExists) {
+if (!fs.existsSync(__dirname + '/../ios/sentry.properties')) {
+  fs.copyFileSync(
+    __dirname + '/../ios/sentry.properties.example',
+    __dirname + '/../ios/sentry.properties',
+  )
+}
+
+if (!fs.existsSync(__dirname + '/../.env')) {
   fs.copyFileSync(__dirname + '/../.env.dev', __dirname + '/../.env')
 }
