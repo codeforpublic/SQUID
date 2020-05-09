380c90029ce7c42f18322801a977ea46e53bbf14
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2020-05-09 15:30:51 +0700

    fix get public key

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,19 +1,21 @@
 import { RSA } from 'react-native-rsa-native'
-import { API_URL, API_KEY, SSL_PINNING_CERT_NAME } from '../config'
+import { API_URL, SSL_PINNING_CERT_NAME } from '../config'
 import { fetch } from 'react-native-ssl-pinning'
+import { userPrivateData } from '../state/userPrivateData'
 
 let publicKey
 export const refetchDDCPublicKey = async () => {
   try {
+    const authToken = userPrivateData.getData('authToken')
     const resp = await fetch(API_URL + '/ddc/public_key', {
       method: 'GET',
-    sslPinning: {
-      certs: [SSL_PINNING_CERT_NAME],
-    },
-    headers: {
-      'X-TH-API-Key': API_KEY,
-      'Content-Type': 'application/json',
-    },
+      sslPinning: {
+        certs: [SSL_PINNING_CERT_NAME],
+      },
+      headers: {
+        Authorization: authToken ? 'Bearer ' + authToken : void 0,
+        'Content-Type': 'application/json',
+      },
     })
     if (resp.status === 200) {
       publicKey = await resp.text()
