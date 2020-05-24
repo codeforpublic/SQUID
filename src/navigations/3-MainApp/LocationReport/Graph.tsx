ece6a516114fd95f0f36f3971b1f5fb327201dec
supagrid tangsermsit <boyisboyis.work@gmail.com>
2020-05-24 21:57:25 +0700

    feat: update code

--- src/navigations/3-MainApp/LocationReport/Graph.tsx
+++ src/navigations/3-MainApp/LocationReport/Graph.tsx
@@ -50,6 +50,7 @@ export const Graph = () => {
 
 
 		const dataTwoWeek = await StoreLocationHistoryService.getDataTwoWeek();
+		console.log('dataTwoWeek: ', dataTwoWeek);
 		if (dataTwoWeek) {
 			console.log(dataTwoWeek);
 			const format = 'YYYYMMDD-00:00';
@@ -90,108 +91,108 @@ export const Graph = () => {
 
 
 
-	const exampleData = () => {
-		return {
-			"20200514-00:00": { "H": 20, "O": 20, "W": 40, "G": 20 },
-			"20200515-00:00": { "H": 20, "O": 10, "W": 50, "G": 20 },
-			"20200516-00:00": { "H": 0, "O": 80, "W": 0, "G": 20 },
-			"20200517-00:00": { "H": 0, "O": 50, "W": 0, "G": 50 },
-			"20200518-00:00": { "H": 50, "O": 0, "W": 0, "G": 50 },
-			"20200519-00:00": { "H": 20, "O": 10, "W": 50, "G": 20 },
-			"20200520-00:00": { "H": 20, "O": 20, "W": 40, "G": 20 },
-		};
-	}
+	// const exampleData = () => {
+	// 	return {
+	// 		"20200514-00:00": { "H": 20, "O": 20, "W": 40, "G": 20 },
+	// 		"20200515-00:00": { "H": 20, "O": 10, "W": 50, "G": 20 },
+	// 		"20200516-00:00": { "H": 0, "O": 80, "W": 0, "G": 20 },
+	// 		"20200517-00:00": { "H": 0, "O": 50, "W": 0, "G": 50 },
+	// 		"20200518-00:00": { "H": 50, "O": 0, "W": 0, "G": 50 },
+	// 		"20200519-00:00": { "H": 20, "O": 10, "W": 50, "G": 20 },
+	// 		"20200520-00:00": { "H": 20, "O": 20, "W": 40, "G": 20 },
+	// 	};
+	// }
 
 	useEffect(() => {
 		getLocationList();
 
-		// let data = transformData(exampleData());
-		let data = transformData(locationTwoWeek);
-		setLocation(prepareForChart(data));
+		// // let data = transformData(exampleData());
+		// let data = transformData(locationTwoWeek);
+		// setLocation(prepareForChart(data));
 	}, [])
 
-	const transformData = (objData: Object) => {
-		if (!objData) return [];
+	// const transformData = (objData: Object) => {
+	// 	if (!objData) return [];
 
-		return Object.keys(objData).map(k => {
-			return {
-				date: k,
-				...objData[k],
-			};
-		}).sort(((a, b) => (a.date > b.date) ? 1 : (a.date === b.date) ? 0 : -1));
-	}
+	// 	return Object.keys(objData).map(k => {
+	// 		return {
+	// 			date: k,
+	// 			...objData[k],
+	// 		};
+	// 	}).sort(((a, b) => (a.date > b.date) ? 1 : (a.date === b.date) ? 0 : -1));
+	// }
 
-	const prepareForChart = (data = []) => {
-		let amtDays = data.length;
-		if (data.length < 14) {
-			for (let i = 0; i < (14 - amtDays); i++) {
-				data.push({ date: "", H: 0, O: 0, W: 0, G: 0 });
-			}
-		}
+	// const prepareForChart = (data = []) => {
+	// 	let amtDays = data.length;
+	// 	if (data.length < 14) {
+	// 		for (let i = 0; i < (14 - amtDays); i++) {
+	// 			data.push({ date: "", H: 0, O: 0, W: 0, G: 0 });
+	// 		}
+	// 	}
 
-		let home = [], other = [], work = [], nogps = [];
-		let sumH = 0, sumO = 0, sumW = 0, sumG = 0;
-		data.forEach((v, i) => {
-			sumH += v.H;
-			sumO += v.O;
-			sumW += v.W;
-			sumG += v.G;
+	// 	let home = [], other = [], work = [], nogps = [];
+	// 	let sumH = 0, sumO = 0, sumW = 0, sumG = 0;
+	// 	data.forEach((v, i) => {
+	// 		sumH += v.H;
+	// 		sumO += v.O;
+	// 		sumW += v.W;
+	// 		sumG += v.G;
 
-			if (i === 0) {
-				if (amtDays < 1) {
-					sumH = 0; sumO = 0; sumW = 0; sumG = 0;
-				}
-				home.push({ "x": "1", "y": sumH });
-				work.push({ "x": "1", "y": sumW });
-				other.push({ "x": "1", "y": sumO });
-				nogps.push({ "x": "1", "y": sumG });
-			}
-			if (i === 2) {
-				if (amtDays < 3) {
-					sumH = 0; sumO = 0; sumW = 0; sumG = 0;
-				}
-				home.push({ "x": "3", "y": sumH / 3 });
-				work.push({ "x": "3", "y": sumW / 3 });
-				other.push({ "x": "3", "y": sumO / 3 });
-				nogps.push({ "x": "3", "y": sumG / 3 });
-			}
-			if (i === 4) {
-				if (amtDays < 5) {
-					sumH = 0; sumO = 0; sumW = 0; sumG = 0;
-				}
-				home.push({ "x": "5", "y": sumH / 5 });
-				work.push({ "x": "5", "y": sumW / 5 });
-				other.push({ "x": "5", "y": sumO / 5 });
-				nogps.push({ "x": "5", "y": sumG / 5 });
-			}
-			if (i === 6) {
-				if (amtDays < 7) {
-					sumH = 0; sumO = 0; sumW = 0; sumG = 0;
-				}
-				home.push({ "x": "7", "y": sumH / 7 });
-				work.push({ "x": "7", "y": sumW / 7 });
-				other.push({ "x": "7", "y": sumO / 7 });
-				nogps.push({ "x": "7", "y": sumG / 7 });
-			}
-			if (i === 13) {
-				if (amtDays < 14) {
-					sumH = 0; sumO = 0; sumW = 0; sumG = 0;
-				}
-				home.push({ "x": "14", "y": sumH / 14 });
-				work.push({ "x": "14", "y": sumW / 14 });
-				other.push({ "x": "14", "y": sumO / 14 });
-				nogps.push({ "x": "14", "y": sumG / 14 });
-			}
-		});
+	// 		if (i === 0) {
+	// 			if (amtDays < 1) {
+	// 				sumH = 0; sumO = 0; sumW = 0; sumG = 0;
+	// 			}
+	// 			home.push({ "x": "1", "y": sumH });
+	// 			work.push({ "x": "1", "y": sumW });
+	// 			other.push({ "x": "1", "y": sumO });
+	// 			nogps.push({ "x": "1", "y": sumG });
+	// 		}
+	// 		if (i === 2) {
+	// 			if (amtDays < 3) {
+	// 				sumH = 0; sumO = 0; sumW = 0; sumG = 0;
+	// 			}
+	// 			home.push({ "x": "3", "y": sumH / 3 });
+	// 			work.push({ "x": "3", "y": sumW / 3 });
+	// 			other.push({ "x": "3", "y": sumO / 3 });
+	// 			nogps.push({ "x": "3", "y": sumG / 3 });
+	// 		}
+	// 		if (i === 4) {
+	// 			if (amtDays < 5) {
+	// 				sumH = 0; sumO = 0; sumW = 0; sumG = 0;
+	// 			}
+	// 			home.push({ "x": "5", "y": sumH / 5 });
+	// 			work.push({ "x": "5", "y": sumW / 5 });
+	// 			other.push({ "x": "5", "y": sumO / 5 });
+	// 			nogps.push({ "x": "5", "y": sumG / 5 });
+	// 		}
+	// 		if (i === 6) {
+	// 			if (amtDays < 7) {
+	// 				sumH = 0; sumO = 0; sumW = 0; sumG = 0;
+	// 			}
+	// 			home.push({ "x": "7", "y": sumH / 7 });
+	// 			work.push({ "x": "7", "y": sumW / 7 });
+	// 			other.push({ "x": "7", "y": sumO / 7 });
+	// 			nogps.push({ "x": "7", "y": sumG / 7 });
+	// 		}
+	// 		if (i === 13) {
+	// 			if (amtDays < 14) {
+	// 				sumH = 0; sumO = 0; sumW = 0; sumG = 0;
+	// 			}
+	// 			home.push({ "x": "14", "y": sumH / 14 });
+	// 			work.push({ "x": "14", "y": sumW / 14 });
+	// 			other.push({ "x": "14", "y": sumO / 14 });
+	// 			nogps.push({ "x": "14", "y": sumG / 14 });
+	// 		}
+	// 	});
 
-		let result = [];
-		result.push(home.reverse());
-		result.push(other.reverse());
-		result.push(work.reverse());
-		result.push(nogps.reverse());
+	// 	let result = [];
+	// 	result.push(home.reverse());
+	// 	result.push(other.reverse());
+	// 	result.push(work.reverse());
+	// 	result.push(nogps.reverse());
 
-		return result;
-	}
+	// 	return result;
+	// }
 
 
 	return (
