d5d6ba228824c81f3b3efac2a0fafd77e7947a5a
Yuttana Kungwon <birdxcite@gmail.com>
2020-07-31 11:24:25 +0700

    fix: :zap: update prebuild script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # Creates an .env from ENV variables for use with react-native-config
-ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
-printf "Creating an .env file with the following whitelist:\n"
-printf "%s\n" $ENV_WHITELIST
-set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
+if [ $APPCENTER_BRANCH == "master"]; then  
+  ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
+  printf "Creating an .env file with the following whitelist:\n"
+  printf "%s\n" $ENV_WHITELIST
+  set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
+fi
+
 printf "\n.env created with contents:\n\n"
 cat .env
