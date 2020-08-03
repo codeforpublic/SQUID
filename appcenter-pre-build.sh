daa151de4006ca74c65b1096cbd265573434a08e
Yuttana Kungwon <birdxcite@gmail.com>
2020-08-03 22:47:37 +0700

    fix: :bug: Update build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -7,7 +7,10 @@ then
   printf "Creating an .env.production file with the following whitelist:\n"
   printf "%s\n" $ENV_WHITELIST
   set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env.production
+  
+  printf "\n.env.production created with contents:\n\n"
+  cat .env.production
+else 
+  printf "\n.env.dev created with contents:\n\n"
+  cat .env.dev
 fi
-
-printf "\n.env.production created with contents:\n\n"
-cat .env.production
