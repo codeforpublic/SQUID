19c3ed769272fa5192cdbe21b6b863c01c39ae19
jade <jadearmy89@gmail.com>
2021-07-21 17:23:41 +0700

    Change change picture logic

--- src/navigations/3-MainApp/NewMainApp/UpdateProfileButton.tsx
+++ src/navigations/3-MainApp/NewMainApp/UpdateProfileButton.tsx
@@ -1,23 +1,33 @@
-import React, {  } from 'react'
-import { StyleSheet, TouchableHighlight, Alert } from 'react-native'
-import FeatureIcon from 'react-native-vector-icons/Feather'
-import 'moment/locale/th'
-import { useNavigation } from 'react-navigation-hooks'
-import { useApplicationState } from '../../../state/app-state'
 import moment from 'moment'
+import 'moment/locale/th'
+import React from 'react'
+import { Alert, StyleSheet, TouchableHighlight } from 'react-native'
+import FeatureIcon from 'react-native-vector-icons/Feather'
+import { useNavigation } from 'react-navigation-hooks'
+import I18n from '../../../../i18n/i18n'
+import { applicationState, useApplicationState } from '../../../state/app-state'
 
-import I18n from '../../../../i18n/i18n';
+// Can change up to 3 picture a week.
+const MAX_CHANGE_PROFILE_LIMIT = 3
 
-const START_PERIODS = 3 // 3 first days, freely change image
-const DEFAULT_PERIODS = 7 // 7 days per time
-export const UpdateProfileButton = ({ width, style, onChange }) => {
+// const START_PERIODS = 3 // 3 first days, freely change image
+// const DEFAULT_PERIODS = 7 // 7 days per time
+export const UpdateProfileButton: React.FC<{
+  onChange: (uri: string) => void
+  width?: number
+  height?: number
+  style?: React.CSSProperties
+}> = ({ width, style, onChange }) => {
   const navigation = useNavigation()
-  const [data] = useApplicationState()
-  const daySinceCreated = moment().diff(data.createdDate, 'days')
-  const daySinceUpdated = moment().diff(data.updateProfileDate, 'days')
-  const isLock = !(
-    daySinceCreated < START_PERIODS || daySinceUpdated >= DEFAULT_PERIODS
+  let [{ updateProfileDate, changeCount }] = useApplicationState()
+
+  const today = moment()
+  const isSameWeek = today.isSame(
+    updateProfileDate || new Date().toISOString(),
+    'weeks',
   )
+  const days = moment().endOf('weeks').diff(today, 'days')
+  const isLock = (changeCount || 0) >= MAX_CHANGE_PROFILE_LIMIT && isSameWeek
 
   return (
     <TouchableHighlight
@@ -26,32 +36,40 @@ export const UpdateProfileButton = ({ width, style, onChange }) => {
       underlayColor="#DDDDDD"
       onPress={() => {
         if (isLock) {
-          const day = DEFAULT_PERIODS - daySinceUpdated
           Alert.alert(
             I18n.t('can_not_change_picture'),
-            'คุณจะสามารถเปลี่ยนรูปได้อีกใน ' + day + I18n.t('day_s'),
+            I18n.t('can_change_pic_again_in') + (days || 1) + I18n.t('day_s'),
           )
         } else {
           navigation.navigate('MainAppFaceCamera', {
-            setUri: uri => {
-              if (daySinceCreated >= 3) {
-                Alert.alert(
-                  I18n.t('are_you_sure'),
-                  `${I18n.t('after_changed_pic_you_will_not_be_able_to_change_until')} ${DEFAULT_PERIODS} ${I18n.t('day_s_have_passed')}`,
-                  [
-                    { text: I18n.t('cancel'), style: 'cancel' },
-                    {
-                      text: I18n.t('confirm'),
-                      onPress: () => {
-                        onChange(uri)
-                      },
-                    },
-                  ],
-                )
-              } else {
-                onChange(uri)
-              }
+            setUri: (uri: string) => {
+              applicationState.setData2({
+                changeCount: (changeCount || 0) + 1,
+                updateProfileDate: new Date().toISOString(),
+              })
+              onChange(uri)
             },
+            // setUri: (uri) => {
+            //   if (daySinceCreated >= 3) {
+            //     Alert.alert(
+            //       I18n.t('are_you_sure'),
+            //       `${I18n.t(
+            //         'after_changed_pic_you_will_not_be_able_to_change_until',
+            //       )} ${DEFAULT_PERIODS} ${I18n.t('day_s_have_passed')}`,
+            //       [
+            //         { text: I18n.t('cancel'), style: 'cancel' },
+            //         {
+            //           text: I18n.t('confirm'),
+            //           onPress: () => {
+            //             onChange(uri)
+            //           },
+            //         },
+            //       ],
+            //     )
+            //   } else {
+            //     onChange(uri)
+            //   }
+            // },
           })
         }
       }}
